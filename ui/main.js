a10511d6000d8ecf525d579c37e7bfc5230533d5
Rojasree-V <30633772+Rojasree-V@users.noreply.github.com>
2017-08-13 17:52:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,7 @@
 console.log('Loaded!');
+//Module P5
 //change the Main Text
-var element = document.getElementById('main-txt');
+/*var element = document.getElementById('main-txt');
 element.innerHTML = "New Value inserted from JS";
 
 //make the image move on click
@@ -15,4 +16,21 @@ img.onclick = function() {
     var interval = setInterval(moveRight, 100);
     //img.style.marginLeft ='500px';
     
+};*/
+
+//Module P6
+var button = document.getElementById('counter');
+
+
+// make a req to the counter endpoint
+var request = new 
+
+//capture the response and store it in a variable
+
+
+// render the variable in the coorect span
+counter = counter +1;
+var span = document.getElementById('count');
+span.innerHTML = counter.toString();
+
 };
