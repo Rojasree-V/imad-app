68e24bf02e02eab97d755123d7145095f14a8d23
Rojasree-V <30633772+Rojasree-V@users.noreply.github.com>
2017-08-13 17:27:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ element.innerHTML = "New Value inserted from JS";
 var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight(){
-        marginLeft = marginLeft + 10;
+        marginLeft = marginLeft + 1;
         img.style.marginLeft = marginLeft + 'px';
 }
 
