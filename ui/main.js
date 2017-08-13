96ded96652ea9e0ae871bcb12d1fbeaab1980373
Rojasree-V <30633772+Rojasree-V@users.noreply.github.com>
2017-08-13 18:08:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,9 @@
 console.log('Loaded!');
+/*
 //Module P5
+---------------------------------------------------------
 //change the Main Text
-/*var element = document.getElementById('main-txt');
+var element = document.getElementById('main-txt');
 element.innerHTML = "New Value inserted from JS";
 
 //make the image move on click
@@ -16,21 +18,39 @@ img.onclick = function() {
     var interval = setInterval(moveRight, 100);
     //img.style.marginLeft ='500px';
     
-};*/
+};
+----------------------------------------------------------
+*/
 
-//Module P6
+
+//Module P6 API
+
 var button = document.getElementById('counter');
 
-
-// make a req to the counter endpoint
-var request = new 
-
-//capture the response and store it in a variable
-
-
+button.onclick = function (){
+        // create a req obj
+        var request = new XMLHttpRequest();
+        
+        //capture the response and store it in a variable
+        request.onreadystatechange = function () {
+            if(request.readyState == XMLHttpRequest.DONE){
+                //Take some action - to render the span variable count
+                if (request.status == 200){
+                    var counter = request.responseText;
+                    var span = document.getElementById('count');
+                    span.innerHTML = counter.toString();
+                }
+            }
+            
+        };
+        
+// make the request
+        reuest.open('GET','http://rojasreev.imad.hasura-app.io/', true);
+       request.send(null);     
+/*
 // render the variable in the coorect span
 counter = counter +1;
 var span = document.getElementById('count');
 span.innerHTML = counter.toString();
-
+*/
 };
