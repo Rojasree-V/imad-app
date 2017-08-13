f946fae281b1e5db6c0322ddd40c512fa8d8d181
Rojasree-V <30633772+Rojasree-V@users.noreply.github.com>
2017-08-13 18:20:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -28,6 +28,11 @@ app.get('/article-three', function (req, res) {
     res.send('Article three is requested and will be serverd here');
 });
 
+var counter=0;
+app.get('/counter', function(req,res) {
+    counter = counter+1;
+    res.send(counter.toString());
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
