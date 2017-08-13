0f528314b9b9c4a8b88f81abd3ad226a006f8820
Rojasree-V <30633772+Rojasree-V@users.noreply.github.com>
2017-08-13 17:25:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,7 @@ element.innerHTML = "New Value inserted from JS";
 
 //make the image move on click
 var img = document.getElementById('madi');
+var marginLeft = 0;
 function moveRight(){
         marginLeft = marginLeft + 10;
         img.style.marginLeft = marginLeft + 'px';
