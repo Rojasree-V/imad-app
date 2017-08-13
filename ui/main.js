488f1e0f930f4b5f8e64113f1220f08abe9f530a
Rojasree-V <30633772+Rojasree-V@users.noreply.github.com>
2017-08-13 17:18:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,10 @@ console.log('Loaded!');
 //change the Main Text
 var element = document.getElementById('main-txt');
 element.innerHTML = "New Value inserted from JS";
+
+//make the image move on click
+var element = document.getElementById('madi');
+img.onclick = function() {
+    img.style.marginLeft ='50px';
+    
+};
