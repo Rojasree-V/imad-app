53556ee1d25e3263793c0c0e59599a0ac4d809c3
Rojasree-V <30633772+Rojasree-V@users.noreply.github.com>
2017-08-14 15:34:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -54,3 +54,22 @@ var span = document.getElementById('count');
 span.innerHTML = counter.toString();
 */
 };
+
+
+
+
+//Submit Name
+var nameInput = document.getElementById("name");
+var name = nameInput.value;
+var submit = document.getElementById("submit_btn");
+submit.onClick = function() {
+     //make a request to the server and send the name
+     //capture a list of names and render it as a list
+     var names = ['name1','name2', 'name3'];
+     var list = '';
+     for (i=0; i<name.length; i++ ) {
+      lsit = '<li>' + names[i] + '</li>';
+     }
+ };
+ 
+
