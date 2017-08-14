7e8a1af9aed2963258f60f1332937074e65331b4
Rojasree-V <30633772+Rojasree-V@users.noreply.github.com>
2017-08-14 16:03:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -62,16 +62,16 @@ span.innerHTML = counter.toString();
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
-submit.onClick = function() { alert("you clicked me :-()")
-     //make a request to the server and send the name
+submit.onClick = function() { alert("you clicked me :-()") };
+/*     //make a request to the server and send the name
      //capture a list of names and render it as a list
      var names = ['name1','name2', 'name3', 'name4'];
      var list = '';
-     for (var i=0; i<names.length; i++ ) {
+     for (var i=0; i < names.length; i++ ) {
       lsit = '<li>' + names[i] + '</li>';
      }
      var ul = document.getElementById('nameList');
      ul.innerHTML = list;
- };
+ };  */
  
 
